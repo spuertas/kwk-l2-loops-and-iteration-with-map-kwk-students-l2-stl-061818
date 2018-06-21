ea1dbd2a9de1d702a0cc87bd2d01a5dc66a15448
Sophia <spuertas@micds.org>
2018-06-21 20:13:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file.
+
+let studentArray = ['Mary', 'Joan', 'Eva', 'Frida', 'Taylor', 'Agnes'];
+
+function other(students) {
+  return students.toLowerCase();
+}
+
+function lowerCaseStudentNames(names) {
+  return names.map(other);
+}
+
+function firstAndLast (student) {
+    const studentFirst = student.split(' ')[0];
+    const studentLast = student.split(' ')[1];
+    return { firstName: studentFirst, lastName: studentLast}; 
+}
+function nameToAttributes(hi){
+  return hi.map(firstAndLast);
+}// Code your solution in this file.
+
+let studentArray = ['Mary', 'Joan', 'Eva', 'Frida', 'Taylor', 'Agnes'];
+
+function other(students) {
+  return students.toLowerCase();
+}
+
+function lowerCaseStudentNames(names) {
+  return names.map(other);
+}
+
+function firstAndLast (student) {
+    const studentFirst = student.split(' ')[0];
+    const studentLast = student.split(' ')[1];
+    return { firstName: studentFirst, lastName: studentLast}; 
+}
+function nameToAttributes(hi){
+  return hi.map(firstAndLast);
+}
